af03e0e703bdfbebca3f18a3bc3a207f0ae88616
Sasa Popovic <sasa.popovic.prog@gmail.com>
2025-07-15 16:11:40 +0200

    Moved files + gitignore

---
+++ js/indexJQ.js
@@ -0,0 +1,260 @@
+
+
+
+$(document).ready(function(){
+
+/*---------------Pojavljivanje slike----------------*/
+  function fIn() {
+    $('#sasa').fadeIn();
+  }
+  function fOut() {
+    $('#sasa').fadeOut();
+  }
+  function skloni() {
+    $('#sasa').css('transition', 'box-shadow 0.5s ease-in-out');
+    $('#sasa').css('box-shadow', '0 0 0 #fff');
+  }
+
+
+  function OtkrijSasu() {
+    $('#container').css('visibility', 'hidden');
+    $('#sasa').css('transition', 'opacity 0.5s');
+    $('#sasa').css('opacity', '1')
+    $('#sasa').addClass('sijanje');
+    setTimeout(skloni, 500);
+    
+  }
+
+  setTimeout(OtkrijSasu , 400);
+/*----------------------------------------------------*/
+
+    var homePos = $("#pocetna").offset().top;
+    var aboutPos = $("#omeni").offset().top;
+    var portfolioPos = $("#projekti").offset().top;
+    var contactPos = $("#kontakt").offset().top;
+
+
+
+   /*Nestajanje slike*/
+   $(document).scroll(function() {
+      homePos = $("#pocetna").offset().top;
+      aboutPos = $("#omeni").offset().top;
+      portfolioPos = $("#projekti").offset().top;
+      contactPos = $("#kontakt").offset().top;
+
+      var y = $(this).scrollTop();
+      if (y > portfolioPos - 100) {
+      $('#sasa').fadeOut();
+      } else {
+      $('#sasa').fadeIn();
+      }
+   });
+
+   /*Animacija skrolovanja*/
+   $("#home_page").click(function(){
+      $("#start").hide();
+      $("html, body").animate({scrollTop:homePos -50}, 700, function(){
+         $(":focus").blur();
+         $("#start").show();
+         init();
+      });
+      return false;
+   });
+   $("#about_page").click(function(){
+      $("html, body").animate({scrollTop:aboutPos -50}, 700, function(){
+         $(":focus").blur();
+      });
+      return false;
+   });
+   $(".portfolio_page").click(function(){
+      $("html, body").animate({scrollTop:portfolioPos -50}, 700, function(){
+         $(":focus").blur();
+      });
+      return false;
+   });
+   $("#contact_page").click(function(){
+      $("html, body").animate({scrollTop:contactPos -50}, 700, function(){
+         $(":focus").blur();
+      });
+      return false;
+   });
+
+   // Pokretanje SVG animacije
+  var anim1 = $('#anim1'), anim2 = $('#anim2'), anim3 = $('#anim3'), anim4 = $('#anim4'), anim5 = $('#anim5'), anim6 = $('#anim6'), anim7 = $('#anim7'), anim8 = $('#anim8')
+
+  function animacija(x) {
+    x[0].beginElement();
+  }
+
+  function otkrivanjePocetna() {
+    $('.navbar-nav > li:nth-child(2)').find('span').css("visibility", "visible")
+    $('.navbar-nav > li:nth-child(2)').find('svg').css("visibility", "hidden");
+  }
+
+  function otkrivanjeOmeni() {
+    $('.navbar-nav > li:nth-child(3)').find('span').css("visibility", "visible")
+    $('.navbar-nav > li:nth-child(3)').find('svg').css("visibility", "hidden");
+  }
+
+  function otkrivanjeProjekti() {
+    $('.navbar-nav > li:nth-child(4)').find('span').css("visibility", "visible")
+    $('.navbar-nav > li:nth-child(4)').find('svg').css("visibility", "hidden");
+  }
+
+  function otkrivanjeKontakt() {
+    $('.navbar-nav > li:nth-child(5)').find('span').css("visibility", "visible")
+    $('.navbar-nav > li:nth-child(5)').find('svg').css("visibility", "hidden");
+  }
+
+  var flag1 = true;
+
+  $('.navbar-nav > li:nth-child(2)').mouseenter(function() {
+    if(flag1) {
+      flag1 = false;
+      $(this).find('a > span').css("visibility", "hidden");
+      $(this).find('svg').css("visibility", "visible");
+      setTimeout(animacija, 50, anim1);
+      setTimeout(animacija, 250, anim2);
+      setTimeout(otkrivanjePocetna, 1000);
+    }
+    
+    setTimeout(function() { flag1 = true; }, 2000);
+    
+  });
+
+  var flag2 = true;
+
+  $('.navbar-nav > li:nth-child(3)').mouseenter(function() {
+    if(flag2) {
+      flag2 = false;
+      $(this).find('a > span').css("visibility", "hidden");
+      $(this).find('svg').css("visibility", "visible");
+      setTimeout(animacija, 50, anim3);
+      setTimeout(animacija, 250, anim4);
+      setTimeout(otkrivanjeOmeni, 1000);
+    }
+    
+    setTimeout(function() { flag2 = true; }, 2000);
+  });
+
+  var flag3 = true;
+
+  $('.navbar-nav > li:nth-child(4)').mouseenter(function() {
+    if(flag3) {
+      flag3 = false;
+      $(this).find('a > span').css("visibility", "hidden");
+      $(this).find('svg').css("visibility", "visible");
+      setTimeout(animacija, 50, anim5);
+      setTimeout(animacija, 250, anim6);
+      setTimeout(otkrivanjeProjekti, 1000);
+    }
+    
+    setTimeout(function() { flag3 = true; }, 2000);
+    
+  });
+
+  var flag4 = true;
+
+  $('.navbar-nav > li:nth-child(5)').mouseenter(function() {
+    if(flag4) {
+      flag4 = false;
+      $(this).find('a > span').css("visibility", "hidden");
+      $(this).find('svg').css("visibility", "visible");
+      setTimeout(animacija, 50, anim7);
+      setTimeout(animacija, 250, anim8);
+      setTimeout(otkrivanjeKontakt, 1000);
+    }
+    
+    setTimeout(function() { flag4 = true; }, 2000);
+    
+  });
+
+  
+/*Prozor sa statistikom*/
+        var moveLeft = 20;
+        var moveDown = 10;
+        
+        $('a#trigger').hover(function(e) {
+          $('div#pop-up').show("slow");
+          //.css('top', e.pageY + moveDown)
+          //.css('left', e.pageX + moveLeft)
+          //.appendTo('body');
+        }, function() {
+          $('div#pop-up').hide();
+        });
+        
+        $('a#trigger').mousemove(function(e) {
+          $("div#pop-up").css('top', e.pageY + moveDown).css('left', e.pageX + moveLeft);
+        });
+
+//Guranje sadrzaja na dole pri otvaranju togle dugmeta menija
+        $(".collapse").on('show.bs.collapse', function(){
+            $('body').animate({
+                marginTop: "227px"
+            }, 300);
+            // $('#pocetna').css("background-position", "top 227px");
+            $('.navbar-nav').css("overflow", "hidden");
+            $('.navbar-nav > li:nth-child(n + 2):nth-child(-n + 5) > svg').css("left", "42%");
+            $('.dropdown-content > a').css("left","250%");
+            $('.dropdown-content > a').css("top","-46px");
+        });
+        $(".collapse").on('hide.bs.collapse', function(){
+            $('body').animate({
+                marginTop: "0px"
+            }, 240);
+            // $('#pocetna').css("background-position", "top 0");
+            $('.navbar-nav').css("overflow", "initial");
+            $('.navbar-nav > li:nth-child(n + 2):nth-child(-n + 5) > svg').css("left", "0");
+            $('.dropdown-content > a').css("left","0");
+            $('.dropdown-content > a').css("top","0");
+        });    
+        
+
+//Ovo hendluje ako dodamo marginTop, a izadjemo iz meni kolapsa
+  $( window ).resize(function() {
+    if($(window).width() > 768) {
+      $('body').css("margin-top", "0px")
+      $('.navbar-collapse').collapse('hide');
+    } 
+  }); 
+  //----------------------------------------------------------------   
+
+
+//Dodavanje sjaja elementu
+$('#skolica').hover(
+       function(){ $(this).addClass('sijanje') },
+       function(){ $(this).removeClass('sijanje') }
+)
+$('#crveno').hover(
+      function(){ $('#sasa').css('transition', 'box-shadow 0.5s ease-in-out');
+      $('#sasa').css('box-shadow', '0 0 50px red'); },
+      function(){ $('#sasa').css('transition', 'box-shadow 0.5s ease-in-out');
+      $('#sasa').css('box-shadow', '0 0 0 #fff'); }
+)
+
+$('#plavo').mouseenter(
+      function(){ 
+        $('.navbar-nav > li:nth-child(2) span').addClass('sijanjePlavo');
+        setTimeout(function(){ 
+          $('.navbar-nav > li:nth-child(2) span').removeClass('sijanjePlavo');
+        }, 300);
+        setTimeout(function(){
+          $('.navbar-nav > li:nth-child(3) span').addClass('sijanjePlavo');}, 300);
+        setTimeout(function(){ 
+          $('.navbar-nav > li:nth-child(3) span').removeClass('sijanjePlavo');
+        }, 600);
+        setTimeout(function(){
+          $('.navbar-nav > li:nth-child(4) span').addClass('sijanjePlavo');}, 600);
+        setTimeout(function(){ 
+          $('.navbar-nav > li:nth-child(4) span').removeClass('sijanjePlavo');
+        }, 900);
+        setTimeout(function(){
+          $('.navbar-nav > li:nth-child(5) span').addClass('sijanjePlavo');}, 900);
+        setTimeout(function(){ 
+          $('.navbar-nav > li:nth-child(5) span').removeClass('sijanjePlavo');
+        }, 1200);
+      }
+      
+)
+
+});
---
+++ js/indexJS.js
@@ -0,0 +1,159 @@
+
+window.onload = start;
+window.onresize = promena;
+
+function start(){
+	// kanvas();
+	// var elniz = Array.prototype.slice.call(document.getElementsByClassName('navbar-nav'))
+	// elNiz.forEach(function(item){
+ //    	console.log(item);
+	// });
+	promena();
+	init();
+	initProjekti();
+}
+
+var lastScrollTop = 0;
+var skrivanje
+var sasaslika
+var foot
+
+
+// var c, ctx, ctx1
+
+// function kanvas() {
+// 	c = document.getElementById("myCanvas");
+
+// 	ctx = c.getContext("2d");
+// 	ctx1 = c.getContext("2d");
+	
+// 	// initCanvas();
+// }
+
+// function initCanvas() {
+// 	window.addEventListener('resize', resizeCanvas, false);
+// 	// window.addEventListener('scroll', resizeCanvas, false);
+//     resizeCanvas();
+// }
+// function redraw() {
+// 	c = document.getElementById("myCanvas");
+// 	var elemStart = document.getElementById('start');
+// 	var elemSasa = document.getElementById('sasa');
+
+// 	ctx = c.getContext("2d");
+// 	ctx1 = c.getContext("2d");
+
+// 	ctx.beginPath(); 
+// 	ctx.lineWidth = "2";
+// 	ctx.strokeStyle = "blue"; 
+// 	ctx.moveTo(parseInt(elemStart.style.left, 10),parseInt(elemStart.style.top, 10));
+// 	ctx.lineTo(50, 50);
+// 	// ctx.lineTo(parseInt(elemSasa.style.left, 10),parseInt(elemSasa.style.top, 10));
+// 	ctx.stroke();
+
+// 	ctx1.beginPath();
+// 	ctx1.strokeStyle = "red";
+// 	ctx.moveTo(parseInt(elemStart.style.left, 10),parseInt(elemStart.style.top, 10));
+// 	ctx1.lineTo(1000,30); 
+// 	ctx1.stroke();
+// }
+// function resizeCanvas() {
+// 	c.width = window.innerWidth;
+//     c.height = window.innerHeight;
+//     redraw();
+// }
+
+
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+/*var eventList = ["scroll", "resize"];
+for(event of eventList) {
+    element.addEventListener(event, function() {
+        
+    });
+}*/
+
+
+window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+	if(window.innerWidth > 768) {
+	   var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+	   var oP = window.pageYOffset + document.getElementById('omeni').getBoundingClientRect().top -50
+	   var pP = window.pageYOffset + document.getElementById('projekti').getBoundingClientRect().top -50
+	   var kP = Math.floor(window.pageYOffset + document.getElementById('kontakt').getBoundingClientRect().top -50)
+	   /// var aPoz = aP.getBoundingClientRect().bottom - 100
+	   	
+
+	   	if (st < lastScrollTop || Math.abs(st - oP) < 5 || Math.abs(st - pP) < 5 || Math.abs(st - kP) < 5){
+	   		skryvanje = document.getElementById('meni')
+	    	skryvanje.classList.remove('menigore')
+	    	skryvanje.classList.add('menidole')
+	    	sasaslika = document.getElementById('sasa')
+	    	sasaslika.classList.remove('sasagore')
+	    	sasaslika.classList.add('sasadole')
+	    	foot = document.getElementById('foot')
+	   		foot.classList.remove('footgore')
+	   		foot.classList.add('footdole')   
+	   } else {
+	   		skryvanje = document.getElementById('meni')
+	   		skryvanje.classList.remove('menidole')
+	   		skryvanje.classList.add('menigore')
+	   		sasaslika = document.getElementById('sasa')
+	   		sasaslika.classList.remove('sasadole')
+	   		sasaslika.classList.add('sasagore')
+	   		foot = document.getElementById('foot')
+	   		foot.classList.remove('footdole')
+	   		foot.classList.add('footgore')		
+	   }
+	   
+	   lastScrollTop = st;
+   	}
+   
+}, false);
+
+		
+
+		// function flicker() {
+		// 	var element = document.getElementById('start');
+		//   var treperenje = setInterval(function(){element.toggleClass('hidden');}, 200);
+		//   	setTimeout(function(){clearInterval(treperenje);}, 1000);
+		//
+		// }
+
+
+		function stanje(broj){
+			if(broj===1){
+				document.getElementById('start').style.visibility = "hidden";
+			}
+			else if(broj===2){
+				document.getElementById('start').style.visibility = "visible";
+			}
+		}
+
+		function init(){
+			var counter = 0
+			var funkcija = function(){
+				counter++;
+				var brj = 2;
+				brj = Math.floor(Math.random()*2)+1;
+				stanje(brj);
+				var rand = Math.round(Math.random() * (100 - 50)) + 50;
+				if(counter>40 && brj===2)
+		    	return;
+				else
+					setTimeout(funkcija, rand);
+
+			}
+			funkcija();
+		}
+		
+		function promena(){
+			var elemStart = document.getElementById('start');
+			var posStart = elemStart.getBoundingClientRect();
+			var sirina = posStart.width;
+			var visina = posStart.height;
+
+			elemStart.style.visibility = "visible";
+			elemStart.style.top = window.innerHeight/2 - visina/2 + "px";
+			elemStart.style.left = window.innerWidth/2 - sirina/2 + "px";
+		}
+
+
